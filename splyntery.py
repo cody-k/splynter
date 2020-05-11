a4e8e88b0e9e7e5cc20f79a4b6cb59b413b0740d
cody-k <13126350+cody-k@users.noreply.github.com>
2020-05-11 07:56:10 -0500

    main file name change
    fixed name of runner file

--- splyntery.py
+++ splyntery.py
@@ -1,7 +1,7 @@
 # python3
 #  
 # Author: Cody Keller
-# Description: Progress through Dead by Daylight's Bloodweb in a goal-oriented manner
+# Description: https://www.github.com/cody-k
 ######################################################################################
 
 # IMPORTS
@@ -14,19 +14,20 @@ user32 = ctypes.windll.user32
 scrWidth, scrHeight = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 winWidth, winHeight = 350, 230
 
+# Main Class
 class MainWindow(wx.Frame):
 
-
+	# Initializer
 	def __init__(self, parent, title):
 		wx.Frame.__init__(self, parent, title=title, size=(winWidth,winHeight), style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER ^ wx.MAXIMIZE_BOX)
 		self.Center()
 
-		# MENU
+		# Create File Menu & Set Icon for Splyntery
 		filemenu = wx.Menu()
 		iconImage = wx.Icon()
-		iconImage.CopyFromBitmap(wx.Bitmap("icon.png", wx.BITMAP_TYPE_ANY))
+		iconImage.CopyFromBitmap(wx.Bitmap("images/png/icon.png", wx.BITMAP_TYPE_ANY))
 
-		# ABOUT AND EXIT
+		# About and Exit
 		menuAbout = filemenu.Append(wx.ID_ABOUT, "&About","Information about this program")
 		filemenu.AppendSeparator()
 		menuExit = filemenu.Append(wx.ID_EXIT, "&Exit","Terminate the program")
@@ -38,16 +39,16 @@ class MainWindow(wx.Frame):
 
 		
 
-		# MAIN PANEL
+		# Main Panel
 		pygame.mixer.init(48000, -16, 4, 1024)
-		pygame.mixer.music.load("0004.mp3")
+		pygame.mixer.music.load("audio/0004.mp3")
 		pygame.mixer.music.set_volume(0.2)
 		panel = wx.Panel(self)
 		self.SetBackgroundColour(wx.Colour(35, 37, 48))
-		self.startImage = wx.Image("startbutton.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+		self.startImage = wx.Image("images/png/startbutton.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
 		self.startButton = wx.BitmapButton(self , -1, self.startImage, pos=(winWidth-122,0), style=wx.NO_BORDER) #winHeight-112
 		self.startButton.SetBackgroundColour(wx.Colour(35, 37, 48))
-		self.dbdTitle = wx.Image("dbd.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+		self.dbdTitle = wx.Image("images/png/dbd.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
 		self.bmp1 = wx.StaticBitmap(self, -1, self.dbdTitle, (10,10))
 		self.statusBar = self.CreateStatusBar(style=wx.BORDER_NONE)
 		self.statusBar.SetStatusStyles([wx.SB_FLAT])
@@ -55,10 +56,7 @@ class MainWindow(wx.Frame):
 		self.statusBar.SetForegroundColour(wx.Colour(255,255,255))
 		self.statusBar.SetStatusText("waiting for commands...")
 
-
-		
-		# startButton.SetBitmap(wx.Image("startbuttonhover.png", wx.BITMAP_TYPE_PNG))
-		# Choices
+		# Radio Box -- May change to single rbuttons due to lack of wx.SetForegroundColour() intregration
 		lblist = ["Perks", "Addons", "Random"]
 		self.rbox = wx.RadioBox(self, label="Method of Progress", pos = (10,50), choices = lblist, style = wx.RA_SPECIFY_ROWS)
 		self.rbox.SetForegroundColour(wx.Colour(255, 255, 255))
@@ -97,17 +95,17 @@ class MainWindow(wx.Frame):
 
 
 	def Hover(self, e):
-		newPic = wx.Image("startbuttonhover.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+		newPic = wx.Image("images/png/startbuttonhover.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
 		self.startButton.SetBitmap(newPic)
 		
 
 	def HoverLeave(self, e):
-		newPic = wx.Image("startbutton.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+		newPic = wx.Image("images/png/startbutton.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
 		self.startButton.SetBitmap(newPic)
 
 	
 	# OLD CODE FROM V1 IN USE UNTIL V2 OBJECT DETECTION IS COMPLTED
-	
+
 
 
 
